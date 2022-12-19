f222d90a906bed7dd8c67e9327b73307d61821a8
Haï~ <hai-vr@users.noreply.github.com>
2022-12-19 23:12:00 +0100

    Add more state tests

--- Tests/PlayMode/AnimatorACInternalTest.cs
+++ Tests/PlayMode/AnimatorACInternalTest.cs
@@ -1,4 +1,5 @@
-﻿using NUnit.Framework;
+﻿using AnimatorAsCode.V0;
+using NUnit.Framework;
 using UnityEditor.Animations;
 using UnityEngine;
 using Assert = UnityEngine.Assertions.Assert;
@@ -96,6 +97,98 @@ namespace av3_animator_as_code.Tests.PlayMode
             animator.Update(1 / 60f);
             Assert.IsTrue(Info0(animator).IsName("Second"));
             Assert.IsTrue(child.gameObject.activeSelf); // The object turns back on
+        }
+        
+        [Test]
+        public void It_should_apply_motion_time()
+        {
+            var aac = TestAac();
+            var controller = new AnimatorController();
+            var fx = aac.CreateMainArbitraryControllerLayer(controller);
+
+            // Exercise
+            var child = new GameObject
+            {
+                name = "Child",
+                transform = { parent = _root.transform }
+            };
+            fx.NewState("First")
+                .WithAnimation(aac.NewClip().Animating(clip =>
+                {
+                    clip.Animates(child.transform, "m_LocalScale.x").WithSecondsUnit(keyframes => keyframes.Linear(0f, 0f).Linear(1f, 10f));
+                }))
+                .MotionTime(fx.FloatParameter("MyFloat"));
+
+            var animator = _root.AddComponent<Animator>();
+            animator.runtimeAnimatorController = controller;
+            animator.enabled = false;
+
+            // Verify
+            // Frame 0
+            animator.SetFloat("MyFloat", 0.5f);
+            animator.Update(1 / 60f);
+            Assert.AreEqual(5f, child.transform.localScale.x);
+        }
+        
+        [Test]
+        public void It_should_apply_speed()
+        {
+            var aac = TestAac();
+            var controller = new AnimatorController();
+            var fx = aac.CreateMainArbitraryControllerLayer(controller);
+
+            // Exercise
+            var child = new GameObject
+            {
+                name = "Child",
+                transform = { parent = _root.transform }
+            };
+            fx.NewState("First")
+                .WithAnimation(aac.NewClip().Animating(clip =>
+                {
+                    clip.Animates(child.transform, "m_LocalScale.x").WithSecondsUnit(keyframes => keyframes.Linear(0f, 0f).Linear(1f, 10f));
+                }))
+                .WithSpeed(fx.FloatParameter("MyFloat"));
+
+            var animator = _root.AddComponent<Animator>();
+            animator.runtimeAnimatorController = controller;
+            animator.enabled = false;
+
+            // Verify
+            // Frame 0
+            animator.SetFloat("MyFloat", 2f); // At double the speed
+            animator.Update(15 / 60f); // Advance by a quarter of a second
+            Assert.AreEqual(5f, child.transform.localScale.x);
+        }
+        
+        [Test]
+        public void It_should_apply_speed_set_to_constant()
+        {
+            var aac = TestAac();
+            var controller = new AnimatorController();
+            var fx = aac.CreateMainArbitraryControllerLayer(controller);
+
+            // Exercise
+            var child = new GameObject
+            {
+                name = "Child",
+                transform = { parent = _root.transform }
+            };
+            fx.NewState("First")
+                .WithAnimation(aac.NewClip().Animating(clip =>
+                {
+                    clip.Animates(child.transform, "m_LocalScale.x").WithSecondsUnit(keyframes => keyframes.Linear(0f, 0f).Linear(1f, 10f));
+                }))
+                .WithSpeedSetTo(2f); // At double the speed
+
+            var animator = _root.AddComponent<Animator>();
+            animator.runtimeAnimatorController = controller;
+            animator.enabled = false;
+
+            // Verify
+            // Frame 0
+            animator.Update(15 / 60f); // Advance by a quarter of a second
+            Assert.AreEqual(5f, child.transform.localScale.x);
         }
 
         private static AnimatorStateInfo Info0(Animator animator)
