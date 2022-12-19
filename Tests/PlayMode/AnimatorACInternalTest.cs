9145676a18fd818fcdc239e94485af73bd9fd222
Haï~ <hai-vr@users.noreply.github.com>
2022-12-19 19:16:17 +0100

    Add state test for Write Defaults

--- Tests/PlayMode/AnimatorACInternalTest.cs
+++ Tests/PlayMode/AnimatorACInternalTest.cs
@@ -1,15 +1,14 @@
-﻿using System.Collections;
+﻿using NUnit.Framework;
 using UnityEditor.Animations;
 using UnityEngine;
-using UnityEngine.Assertions;
-using UnityEngine.TestTools;
+using Assert = UnityEngine.Assertions.Assert;
 
 namespace av3_animator_as_code.Tests.PlayMode
 {
-    public class AnimatorInternalACTest : AbstractSimpleSingleLayerAnimatorInternalAC
+    public class AIACStateTest : AbstractSimpleSingleLayerAnimatorInternalAC
     {
-        [UnityTest]
-        public IEnumerator It_should_create_layer_with_state()
+        [Test]
+        public void It_should_create_layer_with_state()
         {
             var aac = TestAac();
             var controller = new AnimatorController();
@@ -25,8 +24,83 @@ namespace av3_animator_as_code.Tests.PlayMode
             Assert.AreEqual(1, animator.layerCount);
             var info = animator.GetCurrentAnimatorStateInfo(0);
             Assert.IsTrue(info.IsName("Init"));
-            Assert.IsTrue(info.IsTag(init.State.tag));
-            yield break;
+        }
+        
+        [Test]
+        public void It_should_cause_write_defaults_OFF_to_preserve_animated_object_state()
+        {
+            var aac = TestAac();
+            var controller = new AnimatorController();
+            var fx = aac.CreateMainArbitraryControllerLayer(controller);
+
+            // Exercise
+            var child = new GameObject
+            {
+                name = "Child",
+                transform = { parent = _root.transform }
+            };
+            var first = fx.NewState("First")
+                .WithWriteDefaultsSetTo(false)
+                .WithAnimation(aac.NewClip().Toggling(child, false));
+            var second = fx.NewState("Second")
+                .WithWriteDefaultsSetTo(false);
+            first.TransitionsTo(second).AfterAnimationFinishes();
+
+            var animator = _root.AddComponent<Animator>();
+            animator.runtimeAnimatorController = controller;
+            animator.enabled = false;
+
+            // Verify
+            // Frame 0
+            animator.Update(0f);
+            Assert.IsTrue(Info0(animator).IsName("First"));
+            Assert.IsFalse(child.gameObject.activeSelf);
+            
+            // Frame 1
+            animator.Update(1 / 60f);
+            Assert.IsTrue(Info0(animator).IsName("Second"));
+            Assert.IsFalse(child.gameObject.activeSelf);
+        }
+        
+        [Test]
+        public void It_should_cause_write_defaults_ON_to_revert_animated_object_state()
+        {
+            var aac = TestAac();
+            var controller = new AnimatorController();
+            var fx = aac.CreateMainArbitraryControllerLayer(controller);
+
+            // Exercise
+            var child = new GameObject
+            {
+                name = "Child",
+                transform = { parent = _root.transform }
+            };
+            var first = fx.NewState("First")
+                .WithWriteDefaultsSetTo(true)
+                .WithAnimation(aac.NewClip().Toggling(child, false));
+            var second = fx.NewState("Second")
+                .WithWriteDefaultsSetTo(true);
+            first.TransitionsTo(second).AfterAnimationFinishes();
+
+            var animator = _root.AddComponent<Animator>();
+            animator.runtimeAnimatorController = controller;
+            animator.enabled = false;
+
+            // Verify
+            // Frame 0
+            animator.Update(0f);
+            Assert.IsTrue(Info0(animator).IsName("First"));
+            Assert.IsFalse(child.gameObject.activeSelf);
+            
+            // Frame 1
+            animator.Update(1 / 60f);
+            Assert.IsTrue(Info0(animator).IsName("Second"));
+            Assert.IsTrue(child.gameObject.activeSelf); // The object turns back on
+        }
+
+        private static AnimatorStateInfo Info0(Animator animator)
+        {
+            return animator.GetCurrentAnimatorStateInfo(0);
         }
     }
 }
